ab2e5eaf79603251386c9ebb9f54af83759e3b9c
Abhinav-Bansal-Groot <abhinav.bansal@grootsoftwares.com>
2025-09-16 14:16:17 +0530

    test changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.api.routes import router as api_router
 
 
-app = FastAPI(title="RAG FastAPI (Clean Architecture)")
+app = FastAPI(title="RAG FastAPI (Clean Architecture) some changes")
 app.include_router(api_router)
 
 
